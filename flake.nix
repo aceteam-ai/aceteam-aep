277091e069c21d9d5efad01585a9fd098956e77e
Justin Xu <justin@adanomad.com>
2026-04-15 20:03:45 +0000

    Add transformers dependencies to nix

--- flake.nix
+++ flake.nix
@@ -10,6 +10,13 @@
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
+        # Shared libs manylinux/pip wheels expect at load time. Extend this list as needed.
+        pythonWheelRuntimeLibs = with pkgs; [
+          stdenv.cc.cc.lib
+          zlib
+          openssl
+          libffi
+        ];
       in {
         devShells.default = pkgs.mkShell {
           packages = [
@@ -17,9 +24,11 @@
             pkgs.python312
           ];
 
+          # Makes the above store paths visible to ld.so when Python loads extension modules.
           shellHook = ''
             export UV_PYTHON="${pkgs.python312}/bin/python3"
             export UV_PYTHON_PREFERENCE="only-system"
+            export LD_LIBRARY_PATH="${pkgs.lib.makeLibraryPath pythonWheelRuntimeLibs}''${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}"
           '';
         };
       });
